cdf4d6e855b97236b7fbe77b0d4a9774fc3078ea
Sam Mauldin <sam@mauldin.me>
2021-12-02 00:00:31 -0600

    Inline!

--- src/day2.ts
+++ src/day2.ts
@@ -1,47 +1,6 @@
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
-type Commands = {
-  command: string;
-  magnitude: number;
-}[];
-
-const partOne = (parsed: Commands) => {
-  let pos = 0;
-  let depth = 0;
-
-  for (const { command, magnitude } of parsed) {
-    if (command === "forward") {
-      pos += magnitude;
-    } else if (command === "down") {
-      depth += magnitude;
-    } else if (command === "up") {
-      depth -= magnitude;
-    }
-  }
-
-  return depth * pos;
-};
-
-const partTwo = (parsed: Commands) => {
-  let pos = 0;
-  let depth = 0;
-  let aim = 0;
-
-  for (const { command, magnitude } of parsed) {
-    if (command === "forward") {
-      pos += magnitude;
-      depth += magnitude * aim;
-    } else if (command === "down") {
-      aim += magnitude;
-    } else if (command === "up") {
-      aim -= magnitude;
-    }
-  }
-
-  return depth * pos;
-};
-
 const main = () => {
   const input = readFileSync(
     resolve(__dirname, "../src/day2.txt"),
@@ -54,9 +13,45 @@ const main = () => {
     return { command, magnitude: parseInt(magnitude) };
   });
 
+  const partOne = () => {
+    let pos = 0;
+    let depth = 0;
+
+    for (const { command, magnitude } of parsed) {
+      if (command === "forward") {
+        pos += magnitude;
+      } else if (command === "down") {
+        depth += magnitude;
+      } else if (command === "up") {
+        depth -= magnitude;
+      }
+    }
+
+    return depth * pos;
+  };
+
+  const partTwo = () => {
+    let pos = 0;
+    let depth = 0;
+    let aim = 0;
+
+    for (const { command, magnitude } of parsed) {
+      if (command === "forward") {
+        pos += magnitude;
+        depth += magnitude * aim;
+      } else if (command === "down") {
+        aim += magnitude;
+      } else if (command === "up") {
+        aim -= magnitude;
+      }
+    }
+
+    return depth * pos;
+  };
+
   return {
-    partOne: partOne(parsed),
-    partTwo: partTwo(parsed),
+    partOne: partOne(),
+    partTwo: partTwo(),
   };
 };
 
